80b47e8e70a2174096341e4abdd84029ad98ca93
tpicst <119066384+tpicst@users.noreply.github.com>
2022-12-18 15:41:17 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const webBody = document.querySelector('html')
+
+const loadingSec = document.querySelector('#loading')
+
+window.addEventListener('load', () => {
+
+    loadingSec.style.display = 'none'
+    
+})
+
